7a45388e3e3e8746fde720874bad12ee1d71192a
st10650 <st10650@iismarconipieralisi.it>
2025-01-15 08:05:14 +0000

    2.1.2

--- src/test_view_requests.php
+++ src/test_view_requests.php
@@ -3,13 +3,12 @@
 
     echo "<br>";
 
-    echo "Visualizzazione della variabile \$_REQUEST con echo:<br>";
-    //non sempre la echo è la scelta giusta!
-    //si aspetta una stringa!
-    echo "$_REQUEST<br>";
-    echo "<br>";
-
     echo "Visualizzazione della variabile \$_REQUEST con print_r():<br>";
     print_r ($_REQUEST);
     echo "<br><br>";    
-?>"
+
+    echo"Visualizzazione della variabile \$_SERVER con var_dump():<br>";
+    print_r ($_SERVER);
+    echo "<br><br>";  
+
+?>
