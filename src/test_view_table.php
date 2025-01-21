1a83b0b5e99fa0ebd661a5d805bfa0bc7f5a3f73
st10650 <st10650@iismarconipieralisi.it>
2025-01-21 08:55:14 +0000

    2.1.4

---
+++ src/test_view_table.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../includes/db.php';
+
+$table = "users";
+$query = "SELECT * FROM $table"
+$result = $connection->query($query);
+
+if($connection->num_rows>0)
+{
+    echo "<table>";
+
+    while($column = $result->fetch_field())
+    {
+        //var_dump($column);
+        echo "<th>";
+        echo $column->name;
+        echo "<br>";
+        echo "</th>";
+    }
+    //stampo contenuto della tabella
+    while ($row = $result->fetch_assoc())
+    {
+        echo "<tr>";
+
+        foreach($row as $key=>$value)
+        {
+            echo "<td> $value  </td>";  
+        }
+        echo "</tr>";
+    }
+   echo "</table>";
+}
